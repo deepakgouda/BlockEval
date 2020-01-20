131364774f382267ec1a4e2bc66580ba4eeb0871
Shashwat <shashwat.jolly@gmail.com>
2020-01-21 00:37:57 +0530

    Implement broadcast pipes

--- simulate.py
+++ simulate.py
@@ -6,15 +6,17 @@ from network import Network
 def simulate(env, params):
 	"""Begin simulation"""
 	net = Network("Blockchain", env, params)
-	net.addMiner(params['numMiners'])
+	net.addPipes(params['numMiners'])
+	net.addMiners(params['numMiners'])
 
 env = simpy.Environment()
 
 params = {}
 params['mu'] = 10
-params['sigma'] = 1
+params['sigma'] = 5
 params['numMiners'] = 2
+params['simulationTime'] = 100
 
 simulate(env, params)
 
-env.run(until = 100)
+env.run(until = params['simulationTime'])
