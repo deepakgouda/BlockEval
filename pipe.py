8745f276d199dd992323cd9c600e9f4d4e689c09
Shashwat <shashwat.jolly@gmail.com>
2020-01-26 16:52:08 +0530

    Implement interrupts on block generation

--- pipe.py
+++ pipe.py
@@ -8,13 +8,21 @@ class Pipe(object):
         self.identifier = identifier
         self.store = simpy.Store(self.env)
 
-    def latency(self, value, delay):
+    def latencyPut(self, value, delay):
         yield self.env.timeout(delay)
         return self.store.put(value)
 
     def put(self, value, srcID):
         delay = 0.5
-        return self.env.process(self.latency(value, delay))
+        return self.env.process(self.latencyPut(value, delay))
+
+    def latencyInterrupt(self, delay, miner):
+        yield self.env.timeout(delay)
+        miner.blockGeneratorAction.interrupt()
+
+    def sendInterrupt(self, miner, srcID):
+        delay = 0.5
+        self.env.process(self.latencyInterrupt(delay, miner))
 
     def get(self):
         return self.store.get()
