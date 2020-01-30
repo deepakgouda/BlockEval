fc128a41b8c9540fbddd4264148e4b0ee1b15862
Shashwat <shashwat.jolly@gmail.com>
2020-01-30 16:11:28 +0530

    Complete transaction pool and flooding

--- miner.py
+++ miner.py
@@ -19,8 +19,7 @@ class Miner:
 		self.pipes = pipes
 		self.miners = miners
 		self.params = params
-		self.transactionPool = TransactionPool(self.env, [0, 1], \
-								"T"+str(self.identifier), self.miners, self.params)
+		self.transactionPool = TransactionPool(env, identifier, neighbourList, miners, params)
 		self.pool = []
 		self.block = []
 		self.parentQueue = []
@@ -31,25 +30,38 @@ class Miner:
 
 	def blockGenerator(self, params):
 		"""Block generator"""
-		transactionCount = int(params["blockCapacity"])
-		transactionList = self.transactionPool.getTransaction(transactionCount)
 
 		while True:
 			try:
-				b = Block(self.currentBlockID, transactionList, params)
 				delay = (self.params['mu']+self.params['sigma']*np.random.\
 													randn(1))[0]
 				yield self.env.timeout(delay)
+
+				transactionCount = int(params["blockCapacity"])
+				transactionList = self.transactionPool.getTransaction(transactionCount)
+				l = []
+				for transaction in transactionList:
+					l.append(transaction.identifier)
+				print("%7.4f"%self.env.now+" : Miner %s proposing block with transaction list: %s" %(self.identifier, l))
+				b = Block("B"+str(self.currentBlockID), transactionList, params)
+
 				if bool(self.params['verbose']):
-					print("%7.4f"%self.env.now+" : Miner%d"%self.identifier+\
-								" generated Block%d"%self.currentBlockID)
+					print("%7.4f"%self.env.now+" : Miner %s"%self.identifier+\
+								" generated Block %s"%b.identifier)
 				self.blockchain.append(b)
 				if bool(self.params['verbose']):
 					self.displayChain()
 					# self.displayLastBlock()
 				# Broadcast block to all neighbours
+				l = []
+				for transaction in transactionList:
+					l.append(transaction.identifier)
+				print("%7.4f"%self.env.now+" : Block %s has transaction list: %s" %(b.identifier, l))
 				broadcast(self.env, b, "Block", self.identifier, self.neighbourList, \
 							self.params, pipes=self.pipes)
+
+				# Remove transactions from local pool
+				self.transactionPool.popTransaction(transactionCount)
 				
 				# Broadcast interrupt to all neighbours
 				broadcast(self.env, "", "Interrupt", self.identifier, self.neighbourList,
@@ -59,7 +71,7 @@ class Miner:
 
 			except simpy.Interrupt:
 				if bool(self.params['verbose']):
-					print("%7.4f"%self.env.now+" : " + "Miner %d"%self.identifier + " interrupted. To mine block %d"%(self.currentBlockID+1) + " now")
+					print("%7.4f"%self.env.now+" : " + "Miner %s"%self.identifier + " interrupted. To mine block %s"%(self.currentBlockID+1) + " now")
 				self.currentBlockID += 1
 
 	def validator(self):
@@ -72,19 +84,24 @@ class Miner:
 		while True:
 			b = yield self.pipes[self.identifier].get()
 			if len(self.blockchain) > 0:
-				currID = self.blockchain[-1].identifier
+				currID = int(self.blockchain[-1].identifier[1:])
 			else:
 				currID = -1
-			if b.identifier <= currID:
+			if int(b.identifier[1:]) <= currID:
 				pass
 				if bool(self.params['verbose']):
-					print("%7.4f"%self.env.now+" : "+"Miner%d"%self.identifier+\
-						" received previous Block%d"%b.identifier)
-			elif b.identifier == currID+1:
+					print("%7.4f"%self.env.now+" : "+"Miner %s"%self.identifier+\
+						" received previous Block %s"%b.identifier)
+			elif int(b.identifier[1:]) == currID+1:
+				"""Remove already mined transactions from private pool"""
+				for transaction in b.transactionList:
+					if transaction in self.transactionPool.transactionList:
+						self.transactionPool.transactionList.remove(transaction)
+				"""Append block to own chain"""
 				self.blockchain.append(b) # to shift to appropriate condition below
 				if bool(self.params['verbose']):
-					print("%7.4f"%self.env.now+" : "+"Miner%d"%self.identifier+\
-						" added Block%d"%b.identifier+" to the chain")
+					print("%7.4f"%self.env.now+" : "+"Miner %s"%self.identifier+\
+						" added Block %s"%b.identifier+" to the chain")
 					self.displayChain()
 					# self.displayLastBlock()
 			else:
@@ -93,18 +110,18 @@ class Miner:
 	def updateBlockchain(self, block):
 		"""Update blockchain by requesting blocks from peers"""
 		if bool(self.params['verbose']):
-			print("%7.4f"%self.env.now+" : "+"Miner%d"%self.identifier+\
-				" updated to Block%d"%block.identifier)
+			print("%7.4f"%self.env.now+" : "+"Miner %s"%self.identifier+\
+				" updated to Block %s"%block.identifier)
 		pass
 
 	def displayChain(self):
 		chain = [b.identifier for b in self.blockchain]
-		print("%7.4f"%self.env.now+" : Miner%d"%self.identifier+\
+		print("%7.4f"%self.env.now+" : Miner %s"%self.identifier+\
 					" has current chain: %s"%chain)
 	
 	def displayLastBlock(self):
 		if self.blockchain:
 			block = self.blockchain[-1]
 			transactions = block.transactionList
-		print("%7.4f"%self.env.now+" : Miner%d"%self.identifier+\
+		print("%7.4f"%self.env.now+" : Miner %s"%self.identifier+\
                     " has last block: %s" % transactions)
--- network.py
+++ network.py
@@ -3,6 +3,8 @@ import numpy as np
 from miner import Miner
 from block import Block
 from pipe import Pipe
+from broadcast import broadcast
+from transaction import Transaction
 
 class Network:
 	"""docstring for Network"""
@@ -10,18 +12,32 @@ class Network:
 		self.name = name
 		self.env = env
 		self.params = params
-		self.miners = []
-		self.pipes = []
+		self.miners = {}
+		self.pipes = {}
+		self.env.process(self.addTransaction())
 
 	def addMiners(self, numMiners):
 		"""Add miner to network"""
 		for identifier in range(numMiners):
-			neighbourList = list(range(identifier)) + list(range(identifier+1, numMiners))
-			self.miners.append(Miner(identifier, self.env,\
-								neighbourList, self.pipes, self.miners, self.params))
+			neighbourList = ["M%d"%x for x in list(range(identifier)) + list(range(identifier+1, numMiners))] 
+			self.miners["M%d"%identifier] = Miner("M%d"%identifier, self.env,\
+																			neighbourList, self.pipes, self.miners, self.params)
 			if bool(self.params['verbose']):
 				print("%7.4f"%self.env.now+" : "+"Miner added")
 
 	def addPipes(self, numMiners, capacity=simpy.core.Infinity):
 		for identifier in range(numMiners):
-			self.pipes.append(Pipe(self.env, identifier))
+			self.pipes["M%d"%identifier] = Pipe(self.env, "M%d"%identifier)
+
+	def addTransaction(self):
+			num = 0
+			while True:
+					delay = (self.params['transactionMu']+self.params['transactionSigma']*np.random.randn(1))[0]
+					yield self.env.timeout(delay)
+					transaction = (Transaction("T%d"%num, self.env.now))
+					if bool(self.params['verbose']):
+							print("%7.4f" % self.env.now+" : " +"%s added" % (transaction.identifier))
+					# Broadcast transactions to all neighbours
+					broadcast(self.env, transaction, "Transaction", "TempID", \
+											["M0", "M1"], self.params, miners=self.miners)
+					num+=1
