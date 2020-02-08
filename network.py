779a0632bb302c62523aef684de684202c9a7883
Deepak Kumar Gouda <deepakgouda1729@gmail.com>
2020-02-08 18:41:25 +0530

    Add randomized connected graph of certain degree and updateChain() (#8)

--- network.py
+++ network.py
@@ -18,29 +18,37 @@ class Network:
 
 	def addMiners(self, numMiners):
 		"""Add miner to network"""
+		# Degree of network graph. Degree >= n/2 guarantees a connected graph
+		degree = numMiners//2
 		for identifier in range(numMiners):
-			neighbourList = ["M%d"%x for x in list(range(identifier)) + list(range(identifier+1, numMiners))] 
+			# Possible neighbours are [0, 1, ... i-1, i+1, ... n]
+			possibleNeighbours = list(range(identifier)) + \
+                    	list(range(identifier+1, numMiners))
+			# Generate a random sample of size degree without replacement from possible neighbours
+			randNeighbour = np.random.choice(possibleNeighbours, size=degree, replace=False)
+			neighbourList = ["M%d"%x for x in randNeighbour]
 			self.miners["M%d"%identifier] = Miner("M%d"%identifier, self.env,\
-																			neighbourList, self.pipes, self.miners, self.params)
+										neighbourList, self.pipes, self.miners, self.params)
 			if bool(self.params['verbose']):
-				print("%7.4f"%self.env.now+" : "+"Miner added")
+				print("%7.4f" % self.env.now+" : "+"%s added with neighbourList %s" %
+					("M%d" % identifier, neighbourList))
 
 	def addPipes(self, numMiners, capacity=simpy.core.Infinity):
 		for identifier in range(numMiners):
 			self.pipes["M%d"%identifier] = Pipe(self.env, "M%d"%identifier)
 
 	def addTransaction(self):
-			num = 0
-			while True:
-				delay = (self.params['transactionMu']+self.params['transactionSigma']*np.random.randn(1))[0]
-				yield self.env.timeout(delay)
-				transaction = (Transaction("T%d"%num, self.env.now))
-				if bool(self.params['verbose']):
-						print("%7.4f" % self.env.now+" : " +"%s added" % (transaction.identifier))
-				# Broadcast transactions to all neighbours
-				broadcast(self.env, transaction, "Transaction", "TempID", \
-					['M0', 'M2'], self.params, miners=self.miners)
-				num+=1
+		num = 0
+		while True:
+			delay = (self.params['transactionMu']+self.params['transactionSigma']*np.random.randn(1))[0]
+			yield self.env.timeout(delay)
+			transaction = (Transaction("T%d"%num, self.env.now))
+			if bool(self.params['verbose']):
+					print("%7.4f" % self.env.now+" : " +"%s added" % (transaction.identifier))
+			# Broadcast transactions to all neighbours
+			broadcast(self.env, transaction, "Transaction", "TempID", \
+				['M0', 'M2'], self.params, miners=self.miners)
+			num+=1
 	
 	def displayChains(self):
 		print("\n--------------------All Miners--------------------\n")
