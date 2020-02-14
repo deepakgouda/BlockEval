d6db374c9654803e2a4dfbb2465fd20264ab66b4
deepakgouda <deepakgouda1729@gmail.com>
2020-02-14 00:16:08 +0000

    Fix latency values. TODO : Update updateChain() function

--- simulate.py
+++ simulate.py
@@ -22,18 +22,3 @@ env = simpy.Environment()
 net = simulate(env, params)
 env.run(until = params['simulationTime'])
 net.displayChains()
-# Simulation time tests : Varying Simulation Time
-# max_n = 100000
-# numPoints = 10
-# lag = max_n//numPoints
-# with open('tests/logs/simTime.log', 'w') as f:
-# 	for n in range(10, max_n+1, lag):
-# 		start = time()
-
-# 		env = simpy.Environment()
-# 		params['simulationTime'] = n
-# 		simulate(env, params)
-# 		env.run(until = params['simulationTime'])
-		
-# 		stop = time()
-# 		f.write("%d %5.5f\n"%(n, stop-start))
--- utils.py
+++ utils.py
@@ -2,7 +2,10 @@ import numpy as np
 import json
 
 def getBlockDelay(mu, sigma):
-    return mu + sigma*np.random.randn()
+    delay = mu + sigma*np.random.randn()
+    if delay < 0:
+        return getBlockDelay(mu, sigma)
+    return delay
 
 
 def getTransmissionDelay(source, destination):
@@ -11,4 +14,7 @@ def getTransmissionDelay(source, destination):
     params = json.loads(params)
     mu = params["delay"][source][destination]["mu"]
     sigma = params["delay"][source][destination]["sigma"]
-    return mu + sigma*np.random.randn()
+    delay = mu + sigma*np.random.randn()
+    if delay < 0:
+        return getTransmissionDelay(source, destination)
+    return delay
